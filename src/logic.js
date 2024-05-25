cec60c708a9eab67ffbe3e047f658c6f11139286
Roman Matveev <roman.matveev@mysky.com>
2024-05-25 23:14:13 +0400

    translate

--- src/logic.js
+++ src/logic.js
@@ -107,7 +107,7 @@ document.addEventListener("DOMContentLoaded", () => {
     if ("serviceWorker" in navigator) {
         window.addEventListener("load", function () {
             navigator.serviceWorker
-                .register("http://localhost:8800/serviceWorker.js")
+                .register("./serviceWorker.js")
                 .then(res => console.log("service worker registered"))
                 .catch(err => console.log("service worker not registered", err));
         });
